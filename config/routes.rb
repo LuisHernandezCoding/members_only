667213d26168975aea5006bb6da58687515d0754
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-12-02 02:10:41 -0600

    - Added Simple Authentication System with has_secure_password - Added Sessions Controller - Added Users Controller - Added Sessions Helper - Added Users Helper - Added User Model - Added Sessions View - Added Users View - Added Sessions Controller Test - Added Users Controller Test - Added Users Fixture - Added User Test - Added User Migration - Added User Schema

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,33 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Add a root route if you don't have one...
+  # We can use users#new for now, or replace this with the controller and action you want to be the site root:
+  root 'users#new'
+
+  # sign up page with form:
+  get 'users/new', to: 'users#new', as: :new_user
+
+  # create (post) action for when sign up form is submitted:
+  post 'users', to: 'users#create'
+
+  # log in page with form:
+  get '/login', to: 'sessions#new'
+
+  # create (post) action for when log in form is submitted:
+  post '/login', to: 'sessions#create'
+
+  # delete action to log out:
+  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
+
+  # TO ACCES THE /LOGOUT ROUTE, YOU NEED TO USE:
+  # <%= link_to 'Log out', logout_path, method: :delete %>
+
+  # now implement the above routes using the resources method:
+  #root 'users#new'
+  #resources :users, only: %i[create]
+  #get 'users/new', to: 'users#new', as: :new_user
+  #resources :sessions, only: %i[new create destroy]
 end
